69b2e80fd2afb4005feced62c2e39be8ab7bd62d
KhantThuHtet <khantthu7007@gmail.com>
2021-11-24 17:35:02 +0630

    done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
+let sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll(".nav-item");
+
 window.onscroll = ()=>{
-    let result = window.scrollY;
-    console.log(window.innerHeight);
-    console.log(result);
+    let current = '';
+
+    sections.forEach( section=>{
+        let sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop - 55) {
+            current = section.getAttribute('id');
+        }
+    });
+
+        navLi.forEach((li) => {
+          li.classList.remove("active");
+          if (li.classList.contains(current)) {
+            li.classList.add("active");
+          }
+        });
+    
+    
 }
 
