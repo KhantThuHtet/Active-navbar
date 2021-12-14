87b740e73bfb283f0cb7fefa96734be167c403c9
KhantThuHtet <khantthu7007@gmail.com>
2021-12-14 12:55:06 +0630

    rev

--- js/app.js
+++ js/app.js
@@ -2,22 +2,23 @@ let sections = document.querySelectorAll('section');
 const navLi = document.querySelectorAll(".nav-item");
 
 window.onscroll = ()=>{
-    let current = '';
-
+    let current;
     sections.forEach( section=>{
         let sectionTop = section.offsetTop;
+        // console.log(sectionTop, scrollY);
         if (scrollY >= sectionTop - 55) {
             current = section.getAttribute('id');
         }
     });
+    // console.log('----------');
 
-        navLi.forEach((li) => {
-          li.classList.remove("active");
-          if (li.classList.contains(current)) {
-            li.classList.add("active");
-          }
-        });
-    
-    
+    // console.log(current);
+    navLi.forEach(li=>{
+        li.classList.remove('active');
+        if (li.classList.contains(current)) {
+              
+            li.classList.add('active');
+        }
+    });
 }
 
